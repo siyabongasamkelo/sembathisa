9212487db2487df8956eb1eb72da060a5645fa96
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-17 19:38:57 +0200

    added global theme provider and react router dom routing

--- client/sembathisa/src/App.js
+++ client/sembathisa/src/App.js
@@ -1,5 +1,55 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "styled-components";
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <div className="App"></div>;
+  const theme = {
+    color: {
+      primary: "#8C00FF",
+      secondary: "#FBE580",
+      tetiary: "rgba(0,0,0,0.7)",
+
+      bigTextColor: "rgba(0,0,0,0.95)",
+      smallTextColor: "rgba(37, 37, 37,0.8)",
+      buttonTextColor: "#D9D9D9",
+    },
+    size: {
+      smallTextDesktop: "18px",
+      mediumTextDesktop: "32px",
+      largeTextDesktop: "42px",
+      logoText: "32px",
+      buttonText: "24px",
+    },
+    gap: {
+      smallGapDesktop: "22px",
+      mediumGapDesktop: "44px",
+      largeGapDesktop: "88px",
+    },
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route index element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/*" element={<HomePage />} />
+      </Route>
+    )
+  );
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
