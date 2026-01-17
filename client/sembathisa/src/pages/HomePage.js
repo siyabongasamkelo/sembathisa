e1ae94890780ab5b62a8011c5db7da5a96218ee5
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-17 19:45:53 +0200

    created home page

---
+++ client/sembathisa/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "../components/homeComponents/Home";
+
+const HomePage = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default HomePage;
