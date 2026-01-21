cb7a848ebfbfc9f723df9df3e07b8fd5d8becbe6
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-21 14:57:42 +0200

    created a theme for the whole web

---
+++ client/sembathisa/src/styles/Theme.js
@@ -0,0 +1,64 @@
+// top page to header gap in px
+
+//1024 -> 54
+//1280 -> 74
+//1440 -> 94
+//1920 -> 104
+
+//links to heroText page
+
+//1024 -> 44
+//1280 -> 64
+//1440 -> 88
+//1920 -> 98
+
+//vertical gap between words
+
+//1024 -> 12
+//1280 -> 16
+//1440 -> 22
+//1920 -> 28
+
+export const appTheme = {
+  colors: {
+    primary: "#8C00FF",
+    secondary: "#FBE580",
+    tertiary: "rgba(0,0,0,0.7)",
+
+    textPrimary: "rgba(0,0,0,0.95)",
+    textSecondary: "rgba(37, 37, 37, 0.8)",
+    buttonText: "#D9D9D9",
+  },
+
+  // fontSizes: { // 1028px sizes
+  //   small: "10px",
+  //   medium: "22px",
+  //   large: "26px",
+  //   logo: "24px",
+  //   button: "24px",
+  // },
+
+  fontSizes: {
+    sm: {
+      small: "10px",
+      medium: "22px",
+      large: "26px",
+      logo: "24px",
+      button: "24px",
+    },
+    md: {
+      small: "14px",
+      medium: "26px",
+      large: "46px",
+      logo: "24px",
+      button: "42px",
+    },
+  },
+
+  spacing: {
+    sm: "8px",
+    md: "12px",
+    lg: "24px",
+    xl: "48px",
+  },
+};
