5f91239654897a2297d871dc1c18aefb9940afc3
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-21 14:54:53 +0200

    moved text file to the typography folder

---
+++ client/sembathisa/src/components/ui/typography/Text.js
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const SmallText = styled.p`
+  font-size: ${(props) => props.theme.fontSizes.sm.small};
+  color: ${(props) => props.theme.colors.textSecondary};
+  font-family: "Kantumruy Pro", sans-serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+  line-height: 22px;
+  text-align: center;
+
+  @media (min-width: 1200px) {
+    font-size: ${(props) => props.theme.fontSizes.md.small};
+  }
+`;
+
+export const HeroText = styled.h1`
+  font-size: ${(props) => props.theme.fontSizes.sm.large};
+  color: ${(props) => props.theme.colors.textPriamry};
+  font-family: "Cormorant Garamond", serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+  text-align: center;
+
+  @media (min-width: 1200px) {
+    font-size: ${(props) => props.theme.fontSizes.md.large};
+  }
+`;
+
+export const MediumText = styled.h3`
+  font-size: ${(props) => props.theme.fontSizes.sm.medium};
+  color: ${(props) => props.theme.colors.textPrimary};
+  font-family: "Cormorant Garamond", serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+  text-align: center;
+
+  @media (min-width: 1200px) {
+    font-size: ${(props) => props.theme.fontSizes.md.medium};
+  }
+`;
+
+export const LogoText = styled.h3`
+  font-size: ${(props) => props.theme.fontSizes.sm.logo};
+  color: ${(props) => props.theme.colors.textPrimary};
+  font-family: "Inspiration", cursive;
+  font-weight: 400;
+  font-style: normal;
+
+  @media (min-width: 1200px) {
+    font-size: ${(props) => props.theme.fontSizes.md.logo};
+  }
+`;
+
+export const ButtonText = styled.h3`
+  font-size: ${(props) => props.theme.fontSizes.sm.button};
+  color: ${(props) => props.theme.colors.buttonText};
+  font-family: "Kantumruy Pro", sans-serif;
+  font-optical-sizing: auto;
+  font-style: normal;
+
+  @media (min-width: 1200px) {
+    font-size: ${(props) => props.theme.fontSizes.md.button};
+  }
+`;
