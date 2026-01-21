d3e6af4846e4f15e36ef2f85826819fae7d3eb6f
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-21 14:56:15 +0200

    added about page to the home page so that it can be accessed by scrolling

--- client/sembathisa/src/pages/HomePage.js
+++ client/sembathisa/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "../components/homeComponents/Home";
+import About from "../components/homeComponents/About";
 
 const HomePage = () => {
   return (
     <>
       <Home />
+      <About />
     </>
   );
 };
