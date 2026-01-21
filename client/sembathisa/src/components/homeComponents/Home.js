6ef7d75e1edcbb20f4bc96ac91bff34f8da8a8ea
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-21 14:50:30 +0200

    added all home page content

--- client/sembathisa/src/components/homeComponents/Home.js
+++ client/sembathisa/src/components/homeComponents/Home.js
@@ -1,15 +1,82 @@
 import React from "react";
-// import { Container } from "../common/Container.styled";
-import { Wrapper } from "./Home.styled";
-import Header from "../common/Header";
-import { Container } from "react-bootstrap";
+import Header from "../layout/Header";
+import { HeroText, MediumText, SmallText } from "../ui/typography/Text";
+import { Button } from "../ui/Button";
+import { Container } from "../ui/Container";
+import {
+  Benefits,
+  Hero,
+  HeroContent,
+  HeroDecorationLeft,
+  HeroDecorationRight,
+  HeroMedia,
+  HeroParagraph,
+  HeroTextContent,
+  HomeLayout,
+  Icons,
+  IconsAndBenefits,
+} from "./Home.styled";
+import family from "../../assets/img/family.jpeg";
+import sewing from "../../assets/img/sewing.jpeg";
+import learning from "../../assets/img/learning.jpeg";
+import {
+  CheckCircleFill,
+  Facebook,
+  Instagram,
+  Twitter,
+  Whatsapp,
+} from "react-bootstrap-icons";
 
 const Home = () => {
   return (
     <Container>
-      <Wrapper>
+      <HomeLayout>
         <Header />
-      </Wrapper>
+        <Hero>
+          <HeroContent>
+            <HeroDecorationLeft></HeroDecorationLeft>
+            <HeroTextContent>
+              <MediumText>SEMBATHISA</MediumText>
+              <HeroText>UBUKHOSI FOUNDATION</HeroText>
+              <HeroParagraph>
+                <SmallText>
+                  Founded with a vison to empower and uplift the youth of South
+                  Africa, the Sembathisa Ubukhosi Foundation is a poineering non
+                  - profit organization
+                </SmallText>
+                <Button>Join Us</Button>
+              </HeroParagraph>
+            </HeroTextContent>
+            <HeroDecorationRight></HeroDecorationRight>
+          </HeroContent>
+          <HeroMedia>
+            <img src={family} alt="peoples" />
+            <img src={sewing} alt="peoples" />
+            <img src={learning} alt="peoples" />
+          </HeroMedia>
+          <IconsAndBenefits>
+            <Icons>
+              <Facebook />
+              <Twitter />
+              <Whatsapp />
+              <Instagram />
+            </Icons>
+
+            <Benefits>
+              <div>
+                <CheckCircleFill /> <SmallText>Skill Development</SmallText>
+              </div>
+              <div>
+                <CheckCircleFill /> <SmallText>Financial Assistance</SmallText>
+              </div>
+              <div>
+                <CheckCircleFill />
+                <SmallText>Community Reintegration</SmallText>
+              </div>
+            </Benefits>
+          </IconsAndBenefits>
+        </Hero>
+      </HomeLayout>
     </Container>
   );
 };
