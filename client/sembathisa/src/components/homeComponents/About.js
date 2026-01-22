447066316b7ef0445476c8480fc35cf251139beb
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-22 11:27:17 +0200

    added aboutus content

--- client/sembathisa/src/components/homeComponents/About.js
+++ client/sembathisa/src/components/homeComponents/About.js
@@ -2,11 +2,19 @@ import React from "react";
 import { Container } from "../ui/Container";
 import {
   AboutLayOut,
+  AboutUsContent,
+  AboutUsContentHeader,
+  AboutUsMedia,
+  AboutUsText,
   Introduction,
   IntroductionHeader,
   IntroductionParagraph,
+  AboutUsContentLayout,
+  AboutUsParagraph,
 } from "./About.styled";
-import { MediumText } from "../ui/typography/Text";
+import { MediumText, SmallMediumText, SmallText } from "../ui/typography/Text";
+import family from "../../assets/img/family.jpeg";
+import { Button } from "../ui/Button";
 
 const About = () => {
   return (
@@ -17,11 +25,48 @@ const About = () => {
             <MediumText>About Sembathisa Ubukhosi Foundation US</MediumText>
           </IntroductionHeader>
           <IntroductionParagraph>
-            The Sembathisa Ubukhosi Foundation is a South African non-profit
-            organisation focused on uplifting, assisting, and re-establishing
-            individuals and communities that were once disadvantaged.
+            <SmallText>
+              The Sembathisa Ubukhosi Foundation is a South African non-profit
+              organisation focused on uplifting, assisting, and re-establishing
+              individuals and communities that were once disadvantaged.
+            </SmallText>
           </IntroductionParagraph>
         </Introduction>
+        <AboutUsContentLayout>
+          <AboutUsContentHeader>
+            <MediumText>What We Do</MediumText>
+          </AboutUsContentHeader>
+
+          <AboutUsContent>
+            <AboutUsMedia>
+              <img src={family} alt="organisation members" />
+            </AboutUsMedia>
+            <AboutUsText>
+              <SmallMediumText>Skills Development</SmallMediumText>
+              <AboutUsParagraph>
+                <SmallText>
+                  We provide practical, market-relevant skills training designed
+                  to improve employability and encourage entrepreneurship. Our
+                  programs focus on preparing youth for real opportunities in
+                  today’s economy.
+                </SmallText>
+              </AboutUsParagraph>
+              <SmallMediumText className="financial">
+                Financial Assistance & Business Support
+              </SmallMediumText>
+              <AboutUsParagraph>
+                <SmallText>
+                  We support small cooperatives and emerging businesses by
+                  connecting them with sponsors, partners, and financial
+                  resources. By strengthening local enterprises, we contribute
+                  to job creation and economic sustainability within
+                  communities.
+                </SmallText>
+              </AboutUsParagraph>
+              <Button>Read More</Button>
+            </AboutUsText>
+          </AboutUsContent>
+        </AboutUsContentLayout>
       </AboutLayOut>
     </Container>
   );
