b38899c50c7b3faf685c65608399beb58d065cb0
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-22 11:35:30 +0200

    removed useless comments and added new sizes to font sizes

--- client/sembathisa/src/styles/Theme.js
+++ client/sembathisa/src/styles/Theme.js
@@ -1,24 +1,3 @@
-// top page to header gap in px
-
-//1024 -> 54
-//1280 -> 74
-//1440 -> 94
-//1920 -> 104
-
-//links to heroText page
-
-//1024 -> 44
-//1280 -> 64
-//1440 -> 88
-//1920 -> 98
-
-//vertical gap between words
-
-//1024 -> 12
-//1280 -> 16
-//1440 -> 22
-//1920 -> 28
-
 export const appTheme = {
   colors: {
     primary: "#8C00FF",
@@ -30,17 +9,10 @@ export const appTheme = {
     buttonText: "#D9D9D9",
   },
 
-  // fontSizes: { // 1028px sizes
-  //   small: "10px",
-  //   medium: "22px",
-  //   large: "26px",
-  //   logo: "24px",
-  //   button: "24px",
-  // },
-
   fontSizes: {
     sm: {
       small: "10px",
+      smallMedium: "16px",
       medium: "22px",
       large: "26px",
       logo: "24px",
@@ -48,6 +20,7 @@ export const appTheme = {
     },
     md: {
       small: "14px",
+      smallMedium: "20px",
       medium: "26px",
       large: "46px",
       logo: "24px",
