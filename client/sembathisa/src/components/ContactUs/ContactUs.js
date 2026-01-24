f78f0b567fb7650941d85b9105240b0788fc2548
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-24 11:01:28 +0200

    feat: contact details and contact form

--- client/sembathisa/src/components/ContactUs/ContactUs.js
+++ client/sembathisa/src/components/ContactUs/ContactUs.js
@@ -1,10 +1,72 @@
+import { FormInput } from "../common/FormInput";
+import { FormTextArea } from "../common/FormTextArea";
+import {
+  Introduction,
+  IntroductionHeader,
+  IntroductionParagraph,
+} from "../layout/PageIntroduction";
+import { Button } from "../ui/Button";
 import { Container } from "../ui/Container";
-import { ContactUsLayout } from "./ContactUs.styled";
+import { FormLabel, SmallMediumText, SmallText } from "../ui/typography/Text";
+import {
+  ContactDetailsContent,
+  ContactDetails,
+  ContactUsContent,
+  ContactUsLayout,
+  ContactForm,
+} from "./ContactUs.styled";
+import { TelephoneFill, GeoAltFill, EnvelopeFill } from "react-bootstrap-icons";
 
 const ContactUs = () => {
   return (
     <Container>
-      <ContactUsLayout></ContactUsLayout>
+      <ContactUsLayout>
+        <Introduction>
+          <IntroductionHeader>
+            <SmallMediumText>Contact Us</SmallMediumText>
+          </IntroductionHeader>
+          <IntroductionParagraph>
+            <SmallText>
+              If you need to reach out to us for any reasons feel free to use
+              any of the details below
+            </SmallText>
+          </IntroductionParagraph>
+        </Introduction>
+        <ContactUsContent>
+          <ContactDetails>
+            <ContactDetailsContent>
+              <TelephoneFill />
+              <SmallText>061 189 2231</SmallText>
+            </ContactDetailsContent>
+            <ContactDetailsContent>
+              <GeoAltFill />
+              <SmallText>
+                KwaZulu-Natal, Durban ,gillespie, 67 whitehaven
+              </SmallText>
+            </ContactDetailsContent>
+            <ContactDetailsContent>
+              <EnvelopeFill />
+              <SmallText>siyabongasamkelociam@gmail.com</SmallText>
+            </ContactDetailsContent>
+          </ContactDetails>
+        </ContactUsContent>
+
+        <ContactForm>
+          <div>
+            <FormLabel>Your Email</FormLabel>
+          </div>
+          <div>
+            <FormInput type="email" placeholder="example@yourmail.com" />
+          </div>
+          <div>
+            <FormLabel>Your Message</FormLabel>
+          </div>
+          <div>
+            <FormTextArea placeholder="Type your message here" />
+          </div>
+          <Button>Submit</Button>
+        </ContactForm>
+      </ContactUsLayout>
     </Container>
   );
 };
