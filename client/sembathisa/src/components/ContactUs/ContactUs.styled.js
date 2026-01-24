5435348747b24ece9713cddf5e818d9117de1136
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-24 08:07:15 +0200

    styles: made page stretch only for a curtain width

---
+++ client/sembathisa/src/components/ContactUs/ContactUs.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ContactUsLayout = styled.div`
+  width: 80%;
+  height: auto;
+  max-width: 1200px;
+
+  margin-top: 60px;
+`;
+
+export const ContactUsContent = styled.div``;
