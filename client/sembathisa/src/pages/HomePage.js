986f4e6817cb3742c2481aa105fd464f35fc2780
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-24 11:05:09 +0200

    chore: adde contactus page

--- client/sembathisa/src/pages/HomePage.js
+++ client/sembathisa/src/pages/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "../components/homeComponents/Home";
 import About from "../components/aboutusComponents/About";
 import OurServices from "../components/ourservices/OurServices";
+import ContactUs from "../components/ContactUs/ContactUs";
 
 const HomePage = () => {
   return (
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Home />
       <About />
       <OurServices />
+      <ContactUs />
     </>
   );
 };
