079da024e233b01168ce12957d2ecc6293efbc45
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-24 08:02:32 +0200

    chore: added container and layoutdiv to give shape and structure

---
+++ client/sembathisa/src/components/ContactUs/ContactUs.js
@@ -0,0 +1,12 @@
+import { Container } from "../ui/Container";
+import { ContactUsLayout } from "./ContactUs.styled";
+
+const ContactUs = () => {
+  return (
+    <Container>
+      <ContactUsLayout></ContactUsLayout>
+    </Container>
+  );
+};
+
+export default ContactUs;
