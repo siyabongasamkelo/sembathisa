036d998a222e0957b7735fa7fb6bdaef5ee38b2a
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-24 11:03:36 +0200

    styles: page and all content responsive

--- client/sembathisa/src/components/ContactUs/ContactUs.styled.js
+++ client/sembathisa/src/components/ContactUs/ContactUs.styled.js
@@ -9,3 +9,37 @@ export const ContactUsLayout = styled.div`
 `;
 
 export const ContactUsContent = styled.div``;
+
+export const ContactDetails = styled.div``;
+
+export const ContactDetailsContent = styled.div`
+  width: 40%;
+  display: flex;
+
+  p {
+    padding-left: 30px;
+    text-align: left;
+  }
+`;
+
+export const ContactForm = styled.form`
+  border: 1px solid #8ca9ff;
+  width: 60%;
+  border-radius: 10px;
+
+  label {
+    margin-left: 10%;
+    margin-top: 20px;
+  }
+
+  input {
+    margin-top: 5px;
+  }
+
+  button {
+    height: 20px;
+    width: 100px;
+    margin-left: 10%;
+    margin-top: 20px;
+  }
+`;
