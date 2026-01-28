95de64ed47e54775caccd62ec2af619130da0567
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-28 17:52:47 +0200

    styles: styled JoinNow page and made spacing dynamic

---
+++ client/sembathisa/src/components/joinNow/JoinNow.styled.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const JoinNowLayout = styled.div`
+  width: 80%;
+  max-width: 1200px;
+
+  margin-top: ${(props) => props.theme.spacing.xxl};
+`;
+
+export const JoinNowContent = styled.div``;
+
+export const JoinCommunity = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+export const JoinCommunityImage = styled.div`
+  img {
+    height: 200px;
+  }
+`;
+
+export const JoinCommunityText = styled.div`
+  h3 {
+    text-align: left;
+    margin-left: 30px;
+  }
+`;
+
+export const JoinCommunityParagraph = styled.div`
+  width: 80%;
+  margin-left: 30px;
+  margin-top: ${(props) => props.theme.spacing.lg};
+
+  p {
+    text-align: left;
+  }
+`;
+
+export const JoinNowButton = styled.div`
+  width: 100%;
+  background-color: black;
+
+  margin-top: ${(props) => props.theme.spacing.lg};
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const JoinNowButtonParagraph = styled.div`
+  width: 30%;
+  margin-left: 30px;
+  p {
+    padding-top: ${(props) => props.theme.spacing.xl};
+    text-align: left;
+    color: ${(props) => props.theme.colors.buttonText};
+  }
+  button {
+    margin-top: ${(props) => props.theme.spacing.md};
+  }
+`;
+
+export const JoinNowButtonImage = styled.div`
+  img {
+    width: 250px;
+    margin-top: ${(props) => props.theme.spacing.xl};
+    margin-right: 60px;
+    margin-bottom: ${(props) => props.theme.spacing.xl};
+  }
+`;
