b1db1de43939f898984e6633f32eaba8c74a7132
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-28 17:51:07 +0200

    styles: styled FAQ page and made spacing dynamic

---
+++ client/sembathisa/src/components/FAQcomponnets/FAQ.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FaqLayout = styled.div`
+  width: 80%;
+  max-width: 1200px;
+  margin-top: ${(props) => props.theme.spacing.xxl};
+`;
+
+export const FaqContent = styled.div`
+  margin-top: ${(props) => props.theme.spacing.xl};
+`;
