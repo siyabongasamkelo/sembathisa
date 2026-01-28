d499979711b9dc50b81318f07894d456b3ef1710
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-28 17:55:23 +0200

    styles: added dynamic spacing

--- client/sembathisa/src/components/layout/PageIntroduction.js
+++ client/sembathisa/src/components/layout/PageIntroduction.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Introduction = styled.div`
-  height: 200px;
   width: 100%;
 
   display: flex;
@@ -12,13 +11,13 @@ export const Introduction = styled.div`
 export const IntroductionHeader = styled.div`
   width: 30%;
   h3 {
-    margin-top: 24px;
   }
   @media (min-width: 1200px) {
-    margin-top: 44px;
   }
 `;
 
 export const IntroductionParagraph = styled.div`
   width: 45%;
+
+  margin-top: ${(props) => props.theme.spacing.sm};
 `;
