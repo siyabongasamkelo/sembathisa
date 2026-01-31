c4980416e8da0df7654797141b1435ccf37f7e4b
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-31 16:58:22 +0200

    style: styled about me page centered titles and left aligned small text

---
+++ client/sembathisa/src/components/aboutusComponents/AboutMobile.styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const AboutMobileLayout = styled.div`
+  width: 80%;
+  margin-top: ${(props) => props.theme.spacing.xxxxl};
+`;
+
+export const AboutContent = styled.div``;
+
+export const AboutHeader = styled.div`
+  margin-top: ${(props) => props.theme.spacing.sm};
+
+  h3 {
+    text-align: left;
+  }
+`;
+
+export const AboutMedia = styled.div`
+  margin-top: ${(props) => props.theme.spacing.lg};
+
+  img {
+    width: 100%;
+  }
+`;
