c565195e1378387b468bbcca3162b1f8933c69e1
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-31 13:03:10 +0200

    styles: made button full width on  mobile devices

--- client/sembathisa/src/components/ui/Button.js
+++ client/sembathisa/src/components/ui/Button.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Button = styled.button`
   height: 40px;
-  width: 70%;
   width: 220px;
   border: none;
   background-color: ${(props) => props.theme.colors.primary};
@@ -10,6 +9,11 @@ export const Button = styled.button`
   font-size: 18px;
   font-weight: 500;
   font-family: "Kantumruy Pro", sans-serif;
+
+  @media only screen and (min-width: 240px) {
+    height: 60px;
+    width: 100%;
+  }
 
   @media (min-width: 1200px) {
     height: 60px;
