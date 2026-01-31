73bb03c919cc09d07ed0720f3c10bf6a666d5775
siyabonga <siyabongasamkelociam@gmail.com>
2026-01-31 13:05:04 +0200

    created container for mobile devices

--- client/sembathisa/src/components/ui/Container.js
+++ client/sembathisa/src/components/ui/Container.js
@@ -4,4 +4,25 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   width: 100vw;
+  @media only screen and (min-width: 240px) {
+    display: none;
+  }
+
+  @media only screen and (min-width: 1000px) {
+    display: flex;
+  }
+`;
+
+export const ContainerMobile = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  @media only screen and (min-width: 240px) {
+    display: block;
+    display: flex;
+  }
+
+  @media only screen and (min-width: 800px) {
+    display: none;
+  }
 `;
