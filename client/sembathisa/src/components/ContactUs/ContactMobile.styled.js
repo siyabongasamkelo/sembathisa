21cd5f3e5216fc710311d582d493ea10ede98a04
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-02 18:54:35 +0200

    style: contact page styles for mobile devices

---
+++ client/sembathisa/src/components/ContactUs/ContactMobile.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const ContactDetails = styled.div``;
+
+export const IconAndText = styled.div`
+  display: flex;
+
+  p {
+    margin-left: 20px;
+    text-align: left;
+  }
+`;
