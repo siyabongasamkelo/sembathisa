12ee13cfc1fd943fa7df6e34dd18dca95e1cbe40
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-02 18:59:29 +0200

    styles: styles for ourservices mobile page

---
+++ client/sembathisa/src/components/ourservices/OurServicesMobile.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const OurServicesContent = styled.div``;
+
+export const BlackScreen = styled.div`
+  background-color: red;
+  width: 100%;
+  height: 300px;
+`;
