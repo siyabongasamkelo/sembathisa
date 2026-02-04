f15fd21baf4e51e190a967cb7d10b860a61598e9
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-04 08:36:05 +0200

    style: styled all form content

--- client/sembathisa/src/components/ContactUs/ContactMobile.styled.js
+++ client/sembathisa/src/components/ContactUs/ContactMobile.styled.js
@@ -10,3 +10,28 @@ export const IconAndText = styled.div`
     text-align: left;
   }
 `;
+
+export const FormMobile = styled.form`
+  border: 1px solid #8ca9ff;
+  border-radius: 10px;
+  margin-top: ${(props) => props.theme.spacing.lg};
+
+  label {
+    margin-top: ${(props) => props.theme.spacing.lg};
+    margin-left: 10%;
+  }
+  input {
+    margin-top: ${(props) => props.theme.spacing.sm};
+  }
+  textarea {
+    margin-top: ${(props) => props.theme.spacing.sm};
+  }
+
+  button {
+    width: 80%;
+    margin-left: 10%;
+    border-radius: 10px;
+    margin-top: ${(props) => props.theme.spacing.lg};
+    margin-bottom: ${(props) => props.theme.spacing.lg};
+  }
+`;
