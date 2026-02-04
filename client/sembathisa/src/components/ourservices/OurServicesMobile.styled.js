7d5c8f818dcde765f3742a67fc1204a8fc9a8389
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-04 08:36:50 +0200

    chore: removed useless code

--- client/sembathisa/src/components/ourservices/OurServicesMobile.styled.js
+++ client/sembathisa/src/components/ourservices/OurServicesMobile.styled.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const OurServicesContent = styled.div``;
 
 export const BlackScreen = styled.div`
-  background-color: red;
   width: 100%;
   height: 300px;
 `;
