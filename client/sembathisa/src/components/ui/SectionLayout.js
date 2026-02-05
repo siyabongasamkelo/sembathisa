2893150efa1fd924a9182ef009706e9a2ec218ca
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-05 11:45:03 +0200

    fix: increases vertical spacing for more white space

--- client/sembathisa/src/components/ui/SectionLayout.js
+++ client/sembathisa/src/components/ui/SectionLayout.js
@@ -3,4 +3,9 @@ import styled from "styled-components";
 export const MobileLayout = styled.div`
   width: 80%;
   margin-top: ${(props) => props.theme.spacing.xl};
+
+  @media only screen and (min-width: 768px) {
+    /* width: 65%; */
+    margin-top: ${(props) => props.theme.spacing.xxxxl};
+  }
 `;
