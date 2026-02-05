c53af263cab3b337840d5ef0bb316fc2f7a5b639
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-05 11:37:19 +0200

    changed font size to the use the systamayic

--- client/sembathisa/src/components/layout/Header.styled.js
+++ client/sembathisa/src/components/layout/Header.styled.js
@@ -21,12 +21,12 @@ export const HeaderInner = styled.div`
 export const LogoArea = styled.div``;
 
 export const NavArea = styled.nav`
-  margin-left: -10px;
+  margin-left: -20px;
   a {
     text-decoration: none;
-    padding-left: 10px;
+    padding-left: 20px;
     color: ${(props) => props.theme.colors.textPrimary};
-    font-size: 12px;
+    font-size: ${(props) => props.theme.fontSizes.sm.smallMedium};
     font-family: "inter";
   }
   @media only screen and (min-width: 240px) {
@@ -44,6 +44,7 @@ export const ActionArea = styled.div`
     border-radius: 30px;
     font-size: 12px;
     margin-top: ${(props) => props.theme.spacing.lg};
+    margin-left: -15px;
   }
 
   @media only screen and (min-width: 240px) {
