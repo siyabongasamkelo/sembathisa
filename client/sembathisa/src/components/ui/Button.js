eb5861ad52759bfdc80d9f2c697f88e0297d1a42
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-07 12:35:57 +0200

    feat: made button glow on hover

--- client/sembathisa/src/components/ui/Button.js
+++ client/sembathisa/src/components/ui/Button.js
@@ -10,6 +10,11 @@ export const Button = styled.button`
   font-weight: 500;
   font-family: "Kantumruy Pro", sans-serif;
 
+  &:hover {
+    box-shadow: 0 0 12px ${(props) => props.theme.colors.primary};
+    transition: 0.5s ease-in-out;
+  }
+
   @media only screen and (min-width: 240px) {
     height: 60px;
     width: 100%;
