58634250075188652c1c567e0fd5f82385ab5287
siyabonga <siyabongasamkelociam@gmail.com>
2026-02-07 18:56:42 +0200

    was testing why mobileheader was not working

--- client/sembathisa/src/components/ui/Container.js
+++ client/sembathisa/src/components/ui/Container.js
@@ -17,15 +17,16 @@ export const ContainerMobile = styled.div`
   display: flex;
   justify-content: center;
   width: 100vw;
-  @media only screen and (min-width: 240px) {
-    display: block;
-    display: flex;
-  }
 
   .homelayout {
     @media only screen and (min-width: 768px) {
       width: 65%;
     }
+  }
+
+  @media only screen and (min-width: 240px) {
+    display: block;
+    display: flex;
   }
   @media only screen and (min-width: 1000px) {
     display: none;
